5ca0c8215ce5041c49caa216f0846a9f7ad4d39a
miyagon <hiim.shu.m@gmail.com>
2020-01-17 02:25:46 +0000

    fix: elapsedMinutes

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -22,18 +22,23 @@ export const fetchLocations = (): Promise<Locations> => {
 interface APIRoom {
   roomName: string;
   isUsing: boolean;
-  timestump: string;
+  timestamp: string;
 }
 
 const translateAPIRoom = (apiResult: APIRoom[]): RoomState[] => {
   const rooms: RoomState[] = [];
 
   apiResult.map(item => {
+    const date = new Date(item.timestamp);
+    const elapsedMinutesFromNow = Math.floor(
+      (Date.now() - date.getTime()) / 60000
+    ); // = ミリ秒 / 1000ミリ秒 / 60秒
+
     rooms.push({
       roomId: item.roomName,
       roomName: item.roomName,
       isUsing: item.isUsing,
-      elapsedMinutes: 10
+      elapsedMinutes: elapsedMinutesFromNow
     });
 
     return item;
