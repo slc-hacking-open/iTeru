fffcfa2e4f228dbc2d81de842c46d428dc0f888e
miyagon <hiim.shu.m@gmail.com>
2020-01-23 06:20:01 +0000

    add: a hamburger menu to Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./header.scss";
+import "./hamburger.css";
 
 export interface HeaderProps {
   showingLocationName?: string;
 }
 
-const Header: FC<HeaderProps> = ({ showingLocationName = "" }) => (
-  <header className="Header">
-    <h1>
-      iTeru<span>{showingLocationName}</span>
-    </h1>
-  </header>
-);
+const Header: FC<HeaderProps> = ({ showingLocationName = "" }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <header className="Header">
+      <h1>
+        iTeru<span>{showingLocationName}</span>
+      </h1>
+      <div
+        className={`menu-trigger ${isActive ? "active" : ""}`}
+        onClick={() => {
+          setIsActive(!isActive);
+        }}
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
