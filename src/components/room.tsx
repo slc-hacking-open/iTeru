57367b55d25f222befdbf73afef2995b22b6b1fb
miyagon <hiim.shu.m@gmail.com>
2020-01-30 05:00:52 +0000

    update: design

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 
 import "./room.scss";
 
@@ -19,12 +19,29 @@ const Room: FC<RoomProps> = ({
     elapsedMinutes > 0 ? Math.floor(elapsedMinutes / 60) : 0;
   const minutes: number = elapsedMinutes > 0 ? elapsedMinutes % 60 : 0;
 
+  let elapsedTimeString = "　";
+  if (isUsing) elapsedTimeString = "　";
+  else if (hours === 0) elapsedTimeString = `${minutes}分`;
+  else elapsedTimeString = `${hours}時間${minutes}分`;
+
+  // 最初だけ隠しておいて、表示アニメーションする
+  // Note: react-transition-group@1.x を使う方がよさげ
+  const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    // 細かいことはわからんが、一瞬待ってやらんとうまくいかぬ
+    setTimeout(() => {
+      setIsShow(true);
+    }, 1);
+  }, []);
+
   return (
-    <div className={`Room ${isUsing ? "-close" : "-open"}`}>
+    <div
+      className={`Room ${isUsing ? "-close" : "-open"} ${
+        isShow ? "-show" : "-hide"
+      }`}
+    >
       <h1 className="Room-roomName">{roomName}</h1>
-      <p className="Room-elapsedTime">
-        {isUsing ? "　" : `${hours}:${minutes}`}
-      </p>
+      <p className="Room-elapsedTime">{elapsedTimeString}</p>
       <img
         className="Room-icon"
         alt="開閉"
