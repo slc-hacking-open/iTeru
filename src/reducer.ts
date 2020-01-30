27c8df77de7c3e293b900db139baab18eae0ad05
miyagon <hiim.shu.m@gmail.com>
2020-01-30 02:07:29 +0000

    fix: rootReducerの型推論

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,12 +1,13 @@
-import { Reducer, combineReducers } from "redux";
+import { combineReducers } from "redux";
 
 import roomsReducer from "./reducers/rooms-reducer";
 import appReducer from "./reducers/app-reducer";
 import uiReducer from "./reducers/ui-reducer";
 
-export const rootReducer: Reducer = combineReducers({
+export const rootReducer = combineReducers({
   rooms: roomsReducer,
   app: appReducer,
   ui: uiReducer
 });
+
 export type RootState = ReturnType<typeof rootReducer>;
